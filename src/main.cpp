4c4567489094579251b838147075f984db88b149
Ackimixs <Ackimixs@protonmail.com>
2024-04-29 13:55:04 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,8 @@ int main(int argc, char* argv[]) {
         port = std::stoi(argv[1]);
     }
     Localization localizer(-1, -1, -1, "127.0.0.1", port);
-    localizer.start();
+    localizer.start();    
+    localizer.sendMessage("strat", "ready", "1");
     //LIDAR connection
     sl::Result<sl::IChannel *> channel = sl::createSerialPortChannel("/dev/USB_LIDAR", 115200);
     sl::ILidarDriver *drv = *sl::createLidarDriver();
